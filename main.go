831c3ae8d03b2cee8aa4374e742bbc4b33c79290
HenryPeeples <hgpeeples48@gmail.com>
2022-08-19 23:24:19 -0500

    [main]: Get bot token

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
+// Package main is the 'driver' of the discord bot
 package main
 
-import "fmt"
+import (
+	_ "fmt"
+	"os"
+
+	"github.com/hankpeeples/go-discord-bot/config"
+	"github.com/withmandala/go-log"
+)
 
 func main() {
-	fmt.Println("Initial commit")
+	// initialize logger
+	var logger *log.Logger = log.New(os.Stdout).WithColor().WithoutTimestamp()
+	// set bot command prefix
+	const PREFIX = ">"
+
+	// Make sure config.json file was given as an argument
+	if len(os.Args) != 2 {
+		logger.Fatalf("Usage: ./{binary} config.json")
+	}
+
+	// Read config file for bot Token
+	Token, err := config.ReadConfig(os.Args[1])
+	if err != nil {
+		logger.Fatalf("ReadConfig(): %v\n", err)
+	}
+
+	logger.Info(Token)
 }
