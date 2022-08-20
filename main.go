d18c0b3aaa37bd9afdaf8f1ecd95d095158783ee
HenryPeeples <hgpeeples48@gmail.com>
2022-08-20 13:59:38 -0500

    [main]: Call bot start

--- main.go
+++ main.go
@@ -5,21 +5,22 @@ import (
 	_ "fmt"
 	"os"
 
+	"github.com/hankpeeples/go-discord-bot/bot"
 	"github.com/hankpeeples/go-discord-bot/utils"
 )
 
 func main() {
-	// set bot command prefix
-	const PREFIX = ">"
-
 	// Make sure config.json file was given as an argument
 	if len(os.Args) != 2 {
 		utils.Logger.Fatalf("Usage: ./{binary} config.json")
 	}
 
 	// Read config file for bot Token
-	_, err := utils.ReadConfig(os.Args[1])
+	token, err := utils.ReadConfig(os.Args[1])
 	if err != nil {
 		utils.Logger.Fatalf("ReadConfig(): %v\n", err)
 	}
+
+	// Start Bot
+	bot.Start(token)
 }
